567968ac972cf9fa537ddfc6d6b7ab9cc1687c6c
carlosdgrafico <carlos.disenadorgrafico@gmail.com>
2018-02-08 16:39:55 -0300

    fancybox

--- app/js/test.js
+++ app/js/test.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+  	$('.banner__scroll').click(function(){
+  	  if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')||location.hostname==this.hostname){
+  	    var
+  	    target=$(this.hash);
+  	    target=target.length?target:$('[name='+this.hash.slice(1)+']');
+  	    if(target.length){
+  	      $('html,body').animate({
+  	          scrollTop:target.offset().top+20
+  	      },1000);
+  	      return false;
+  	    }
+  	  }
+  	});
+
 
    $('.owl-frases, .owl-testimonios').owlCarousel({
       // animateOut: 'slideOutDown',
