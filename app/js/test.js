ccef73dbb56fafc9f6f3f974d29345d0d014a881
carlosdgrafico <carlos.disenadorgrafico@gmail.com>
2018-02-09 04:28:12 -0300

    desktop y mobile, avance

--- app/js/test.js
+++ app/js/test.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  	$('.banner__scroll').click(function(){
+  	$('.banner__scroll, .header__link').click(function(){
   	  if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')||location.hostname==this.hostname){
   	    var
   	    target=$(this.hash);
@@ -13,6 +13,45 @@ $(document).ready(function(){
   	    }
   	  }
   	});
+
+
+   $(window).on('scroll', function(){
+     var $window = $(this);
+     var $altura = $('.banner').height();
+
+     $window.scrollTop() > 200
+       ? $('.banner__scroll').fadeOut(300)
+       : $('.banner__scroll').fadeIn(300);
+
+       $window.scrollTop() > $altura
+         ? $('.header').css('background-color', 'rgba(0,0,0,.3)')
+         : $('.header').css('background-color', 'transparent');
+
+         console.log($altura);
+   });
+
+
+
+
+
+	$('.header__link').click(function(){
+		$(".header__link").removeClass('header__link--active');
+		$(".burger").removeClass('on');
+		$(".header__menu").removeClass('onActive');
+		$(this).addClass('header__link--active');
+
+	});
+
+
+   $(".burger").click(function() {
+	  $(this).toggleClass("on");
+   });
+
+
+   // despliega el menu lateral
+	$('.burger').on('click', function() {
+      $(".header__menu").toggleClass('onActive');
+	});
 
 
    $('.owl-frases, .owl-testimonios').owlCarousel({
