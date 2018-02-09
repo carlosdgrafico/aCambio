84091c036465cf725cf7d5d366ff98b3b15f9a1e
carlosdgrafico <carlos.disenadorgrafico@gmail.com>
2018-02-09 19:14:09 -0300

    animaciones

--- app/js/test.js
+++ app/js/test.js
@@ -15,6 +15,15 @@ $(document).ready(function(){
   	});
 
 
+   $('.banner__scroll').hover(function(){
+		$(this).addClass('animated bounce');
+	});
+
+   $('.banner__scroll').mouseleave(function(){
+		$(this).removeClass('animated bounce');
+	});
+
+
    $(window).on('scroll', function(){
      var $window = $(this);
      var $altura = $('.banner').height();
@@ -30,8 +39,22 @@ $(document).ready(function(){
          console.log($altura);
    });
 
-
-
+   var wow = new WOW(
+     {
+       boxClass:     'wow',      // animated element css class (default is wow)
+       animateClass: 'animated', // animation css class (default is animated)
+       offset:       0,          // distance to the element when triggering the animation (default is 0)
+       mobile:       true,       // trigger animations on mobile devices (default is true)
+       live:         true,       // act on asynchronously loaded content (default is true)
+       callback:     function(box) {
+         // the callback is fired every time an animation is started
+         // the argument that is passed in is the DOM node being animated
+       },
+       scrollContainer: null,    // optional scroll container selector, otherwise use window,
+       resetAnimation: true,     // reset animation on end (default is true)
+     }
+   );
+   wow.init();
 
 
 	$('.header__link').click(function(){
